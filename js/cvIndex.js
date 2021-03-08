319f6fb42eac40604aba1bf33c911c047681ca0c
JaviMun <78433043+JaviMun@users.noreply.github.com>
2021-03-08 13:42:11 -0300

    Add files via upload

---
+++ js/cvIndex.js
@@ -0,0 +1,9 @@
+function date(){
+  let año = new Date();
+  let elemento = document.getElementById("fecha");
+  let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+  let mes = meses[año.getMonth()]
+  elemento.textContent = año.getDate() + " de " + mes + " de " + año.getUTCFullYear();
+}
+
+document.addEventListener("DOMLoadedContent", date());
